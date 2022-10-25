e0321f93fcc1c60b8209392be575108edfdacd35
Daniel <acidistrict@gmail.com>
2022-10-25 21:59:25 +0100

    add city search bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import { useEffect, useState } from "react";
 
 const App = () => {
   const [temp, setTemp] = useState(undefined);
+  const [currentCity, setCurrentCity] = useState("London");
+  const [cityInputValue, setCityInputValue] = useState("");
+
+  const handleCityInput = (event) => {
+    setCityInputValue(event.target.value);
+  };
+
+  const handleCitySubmit = (event) => {
+    event.preventDefault();
+
+    setCurrentCity(cityInputValue);
+  };
 
   console.log(temp);
+  console.log(currentCity);
 
   useEffect(() => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
     )
       .then((res) => {
         if (!res.ok) throw new Error(res.statusText);
@@ -15,9 +28,21 @@ const App = () => {
       })
       .then((data) => setTemp(Math.round(data.main.temp)))
       .catch(console.err);
-  }, []);
+  }, [currentCity]);
 
-  return <div>{temp}º</div>;
+  return (
+    <div>
+      <form onSubmit={handleCitySubmit}>
+        <input
+          onChange={handleCityInput}
+          type="text"
+          placeholder="Search a city..."
+        />
+        <button>Go</button>
+      </form>
+      <div>{temp}º</div>
+    </div>
+  );
 };
 
 export default App;
