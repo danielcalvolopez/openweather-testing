b2bf285020668c0d4f61c339be81b564b215f4d1
Daniel <acidistrict@gmail.com>
2022-10-26 22:22:15 +0100

    trying different ways of fetching location and weather

--- src/App.js
+++ src/App.js
@@ -1,31 +1,75 @@
-import { useContext, useEffect, useState } from "react";
-import { LocationContext } from "./context/LocationContext";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
-  const currentLocation = useContext(LocationContext);
-
-  const [temp, setTemp] = useState(undefined);
-  const [currentCity, setCurrentCity] = useState(currentLocation);
+  const [currentLat, setCurrentLat] = useState("");
+  const [currentLon, setCurrentLon] = useState("");
+  const [currentLocation, setCurrentLocation] = useState("");
+  const [temp, setTemp] = useState("");
   const [cityInputValue, setCityInputValue] = useState("");
+  const [currentCity, setCurrentCity] = useState(currentLocation);
+  const effectRan = useRef(false);
+
+  console.log(currentLocation);
 
   const handleCityInput = (event) => {
     setCityInputValue(event.target.value);
   };
 
-  const handleCitySubmit = (event) => {
-    event.preventDefault();
+  useEffect(() => {
+    if (effectRan.current === false) {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
 
-    setCurrentCity(cityInputValue);
-  };
+      function success(pos) {
+        const crd = pos.coords;
+
+        setCurrentLat(crd.latitude);
+        setCurrentLon(crd.longitude);
+      }
+
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+  }, []);
 
   useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((res) => res.json())
-      .then((data) => setTemp(Math.round(data.main.temp)))
-      .catch((err) => console.log(err));
+    if (effectRan.current === false) {
+      const fetchCity = async () => {
+        const response = await fetch(
+          `https://eu1.locationiq.com/v1/reverse?key=${process.env.REACT_APP_IQ_TOKEN}&lat=${currentLat}&lon=${currentLon}&format=json`
+        );
+
+        const cityData = await response.json();
+        setCurrentLocation(cityData?.address?.city);
+      };
+      fetchCity();
+    }
+  }, [currentLat, currentLon]);
+
+  useEffect(() => {
+    if (effectRan.current === false) {
+      const fetchWeather = async () => {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+        );
+
+        const weatherData = await response.json();
+        setTemp(Math.round(weatherData?.main?.temp));
+      };
+      fetchWeather();
+    }
   }, [currentCity]);
+
+  const handleCitySubmit = (event) => {
+    event.preventDefault();
+    setCurrentCity(cityInputValue);
+  };
 
   return (
     <div>
--- src/context/LocationContext.jsx
+++ src/context/LocationContext.jsx
@@ -9,26 +9,25 @@ export const LocationContextProvider = ({ children }) => {
   const [currentLat, setCurrentLat] = useState("");
   const [currentLon, setCurrentLon] = useState("");
 
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  };
+
+  function success(pos) {
+    const crd = pos.coords;
+
+    setCurrentLat(crd.latitude);
+    setCurrentLon(crd.longitude);
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  navigator.geolocation.getCurrentPosition(success, error, options);
   useEffect(() => {
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
-    };
-
-    function success(pos) {
-      const crd = pos.coords;
-
-      setCurrentLat(crd.latitude);
-      setCurrentLon(crd.longitude);
-    }
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
     fetch(
       `https://eu1.locationiq.com/v1/reverse?key=${process.env.REACT_APP_IQ_TOKEN}&lat=${currentLat}&lon=${currentLon}&format=json`
     )
