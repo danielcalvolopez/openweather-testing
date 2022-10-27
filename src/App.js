28c509a3372f33ecada75472b9f5abe219469e17
Daniel <acidistrict@gmail.com>
2022-10-27 23:39:44 +0100

    my location function implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
+  const formRef = useRef();
   const [temp, setTemp] = useState("");
   const [cityInputValue, setCityInputValue] = useState("");
   const [currentCity, setCurrentCity] = useState("London");
@@ -9,12 +10,33 @@ const App = () => {
   const [maxTemp, setMaxTemp] = useState("");
   const [minTemp, setMinTemp] = useState("");
   const [weather, setWeather] = useState("");
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [currentLocation, setCurrentLocation] = useState("");
 
-  console.log(cityInputValue);
+  console.log(currentLocation);
 
   const handleCityInput = (event) => {
     setCityInputValue(event.target.value);
+  };
+
+  const handleLocation = () => {
+    const successfulLookup = (position) => {
+      const { latitude, longitude } = position.coords;
+      fetch(
+        `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${process.env.REACT_APP_GEO_CODE}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setCurrentLocation(data.results[0].components.city);
+          setCurrentCity(currentLocation);
+        });
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      successfulLookup,
+      console.log
+    );
   };
 
   useEffect(() => {
@@ -36,24 +58,28 @@ const App = () => {
           setMinTemp(Math.round(weatherData?.main?.temp_min));
           setWeather(weatherData?.weather?.[0].main);
         })
-        .catch((err) => setError(err.message));
+        .catch((err) => {
+          setError(true);
+          setErrorMessage(err.message);
+        });
     };
     fetchWeather();
   }, [currentCity]);
 
   const handleCitySubmit = (event) => {
     event.preventDefault();
+    setError(false);
     setCurrentCity(cityInputValue);
     if (cityInputValue === "") {
       return;
     }
-    setCityInputValue("");
+    formRef.current.reset();
   };
 
   return (
     <div>
       <div>
-        <form onSubmit={handleCitySubmit}>
+        <form ref={formRef} onSubmit={handleCitySubmit}>
           <input
             onChange={handleCityInput}
             type="text"
@@ -61,7 +87,9 @@ const App = () => {
           />
           <button>Go</button>
         </form>
-        <button type="submit">My location</button>
+        <button type="submit" onClick={handleLocation}>
+          My location
+        </button>
       </div>
       {!error ? (
         <div>
@@ -76,7 +104,7 @@ const App = () => {
           <div>{weather}</div>
         </div>
       ) : (
-        <div>{error}</div>
+        <div>{errorMessage}</div>
       )}
     </div>
   );
