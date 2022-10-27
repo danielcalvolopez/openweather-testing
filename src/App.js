9fe10ab7b1adb105603dab9eb0e21dc543081d7d
Daniel <acidistrict@gmail.com>
2022-10-27 22:20:53 +0100

    error handling added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ const App = () => {
   const [maxTemp, setMaxTemp] = useState("");
   const [minTemp, setMinTemp] = useState("");
   const [weather, setWeather] = useState("");
-  const [weatherIcon, setWeatherIcon] = useState(undefined);
+  const [error, setError] = useState("");
+
+  console.log(cityInputValue);
 
   const handleCityInput = (event) => {
     setCityInputValue(event.target.value);
@@ -20,17 +22,21 @@ const App = () => {
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
       )
-        .then((response) => response.json())
+        .then((response) => {
+          if (!response.ok) {
+            throw Error("City not found!");
+          }
+          return response.json();
+        })
         .then((weatherData) => {
           setTemp(Math.round(weatherData?.main?.temp));
           setCityData(weatherData?.name);
-          setCountry(weatherData?.sys.country);
+          setCountry(weatherData?.sys?.country);
           setMaxTemp(Math.round(weatherData?.main?.temp_max));
           setMinTemp(Math.round(weatherData?.main?.temp_min));
           setWeather(weatherData?.weather?.[0].main);
-          setWeatherIcon(weatherData?.weather?.[0].icon);
         })
-        .catch((err) => console.error(err));
+        .catch((err) => setError(err.message));
     };
     fetchWeather();
   }, [currentCity]);
@@ -38,27 +44,40 @@ const App = () => {
   const handleCitySubmit = (event) => {
     event.preventDefault();
     setCurrentCity(cityInputValue);
+    if (cityInputValue === "") {
+      return;
+    }
+    setCityInputValue("");
   };
 
   return (
     <div>
-      <form onSubmit={handleCitySubmit}>
-        <input
-          onChange={handleCityInput}
-          type="text"
-          placeholder="Search a city..."
-        />
-        <button>Go</button>
-      </form>
       <div>
-        {cityData}, {country}
+        <form onSubmit={handleCitySubmit}>
+          <input
+            onChange={handleCityInput}
+            type="text"
+            placeholder="Search a city..."
+          />
+          <button>Go</button>
+        </form>
+        <button type="submit">My location</button>
       </div>
-      <div>{temp}º</div>
-      <div>
-        <p>Min: {minTemp}</p>
-        <p>Max: {maxTemp}</p>
-      </div>
-      <div>{weather}</div>
+      {!error ? (
+        <div>
+          <div>
+            {cityData}, {country}
+          </div>
+          <div>{temp}º</div>
+          <div>
+            <p>Min: {minTemp}</p>
+            <p>Max: {maxTemp}</p>
+          </div>
+          <div>{weather}</div>
+        </div>
+      ) : (
+        <div>{error}</div>
+      )}
     </div>
   );
 };
